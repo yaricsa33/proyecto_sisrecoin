3f5a6b4586b67156944d86ad9b6be8033eeb7310
Yaricsa camacho <ycgomez33@gmail.com>
2022-09-02 20:46:11 -0500

    server js fix

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
-
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+const root = path.join(__dirname, "dist", "proyecto-sena");
 
 app.use(express.static(__dirname + "/dist/proyecto-sena"));
 
 app.get("/*", (req, res) => {
-  res.sendFile("/dist/proyecto-sena/index.html");
+  fs.stat(root + req.path, function (err) {
+    if (err) {
+      res.sendFile("index.html", { root });
+    } else {
+      res.sendFile(req.path, { root });
+    }
+  });
 });
 
 app.listen(PORT, () => {
